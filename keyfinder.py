9d2e24d5e5e1ec79b386df2d757225a6f746ab99
Matt Pilgrim <me@mattpilgrim.co.uk>
2020-08-16 17:31:18 +0100

    machine learning (includes notes)

--- keyfinder.py
+++ keyfinder.py
@@ -119,23 +119,228 @@ for target_list in tb.import_json("test.json"):
 
 
 def scaleMatchPercentage(return_array_of_notes,scale):
-    match=0
 
-    for note in return_array_of_notes:
-
-        if note in scale: match = match+1
-
+    if scale==None:
+        print("No Notes")
+    else:
     
-    return str(match)+"/"+str(len(return_array_of_notes))
+        match=0
 
-print(return_array_of_notes)
+        for note in return_array_of_notes:
 
-# B Natural Minor
-print(scaleMatchPercentage(return_array_of_notes,['B','C#/Db', 'D', 'E', 'F#/Gb', 'G','A']))
+            if note in scale: match = match+1
 
-# C Major
-print(scaleMatchPercentage(return_array_of_notes,['C','D', 'E', 'F', 'G', 'A','B']))
 
+    return len(return_array_of_notes)/100*match
+
+
+
+# B Minor - Correct key signature
+scaleMatchPercentage(return_array_of_notes,['B','C#/Db', 'D', 'E', 'F#/Gb', 'G','A'])
+
+# C Major - incorrect key signature
+scaleMatchPercentage(return_array_of_notes,['C','D', 'E', 'F', 'G', 'A','B'])
 
 # D Major - Relative Key
-print(scaleMatchPercentage(return_array_of_notes,['D','E', 'F#/Gb', 'G', 'A', 'B','C#/Db']))
+scaleMatchPercentage(return_array_of_notes,['D','E', 'F#/Gb', 'G', 'A', 'B','C#/Db'])
+
+
+# Scale Intervals - Natural Minor
+
+#Tonic = B (35)
+
+def scale_generator(tonic,intervals):
+    interval = tonic
+    x=0  
+    scale_array = [] 
+
+    if tonic < 116:
+
+        for key in intervals:
+
+            x=x+1
+
+            interval = interval+key
+
+            scale_array.append(midi_range[str(interval)])
+
+        return scale_array
+    
+
+print("---")
+
+#Natural Minor
+
+minor = scale_generator(35,[2,1,2,2,1,2,2])
+
+
+#Natural Major
+
+major = scale_generator(26,[2,2,1,2,2,2,1])
+
+
+# Find the correct or likely match of the natural minor and major key
+
+print("---")
+
+
+for note in {
+        "47":"B",
+        "46":"A#/Bb",
+        "45":"A",
+        "44":"G#/Ab",
+        "43":"G",
+        "42":"F#/Gb",
+        "41":"F",
+        "40":"E",
+        "39":"D#/Eb",
+        "38":"D",
+        "37":"C#/Db",
+        "36":"C"
+}:
+
+    test_note_name = midi_range[str(note)]
+
+    test_note = note
+
+    minor = scale_generator(int(test_note),[2,1,2,2,1,2,2])
+
+    major = scale_generator(int(test_note),[2,2,1,2,2,2,1])
+
+    print(test_note_name+" minor => "+str(scaleMatchPercentage(return_array_of_notes,minor)))
+
+    print(test_note_name+" major => "+str(scaleMatchPercentage(return_array_of_notes,major)))
+ 
+
+    
+    pass
+
+
+
+# We can safely assume that the likely top 80% are going to be the correct scales
+
+print("---")
+
+for note in {
+        "47":"B",
+        "46":"A#/Bb",
+        "45":"A",
+        "44":"G#/Ab",
+        "43":"G",
+        "42":"F#/Gb",
+        "41":"F",
+        "40":"E",
+        "39":"D#/Eb",
+        "38":"D",
+        "37":"C#/Db",
+        "36":"C"
+}:
+
+    test_note_name = midi_range[str(note)]
+
+    test_note = note
+
+    minor = scale_generator(int(test_note),[2,1,2,2,1,2,2])
+
+    major = scale_generator(int(test_note),[2,2,1,2,2,2,1])
+    
+    if scaleMatchPercentage(return_array_of_notes,minor) > 80:
+        test_note_name+" minor => "+str(scaleMatchPercentage(return_array_of_notes,minor))
+
+    if scaleMatchPercentage(return_array_of_notes,major) > 80:
+        test_note_name+" major => "+str(scaleMatchPercentage(return_array_of_notes,major))
+ 
+   
+    pass
+
+# 80 percentage needs to be learnt by the algorithm - take the high percentage then round it to be nearest decimal - 1st wave 
+print("---")
+
+find_highest_percentage_major=[]
+find_highest_percentage_minor=[]
+
+for note in {
+        "47":"B",
+        "46":"A#/Bb",
+        "45":"A",
+        "44":"G#/Ab",
+        "43":"G",
+        "42":"F#/Gb",
+        "41":"F",
+        "40":"E",
+        "39":"D#/Eb",
+        "38":"D",
+        "37":"C#/Db",
+        "36":"C"
+}:
+
+    test_note_name = midi_range[str(note)]
+
+    test_note = note
+
+    minor = scale_generator(int(test_note),[2,1,2,2,1,2,2])
+
+    major = scale_generator(int(test_note),[2,2,1,2,2,2,1])
+
+
+    find_highest_percentage_minor.append(scaleMatchPercentage(return_array_of_notes,minor))
+
+    find_highest_percentage_major.append(scaleMatchPercentage(return_array_of_notes,major))
+
+  
+    pass
+
+max_value_minor = max(find_highest_percentage_minor)
+
+percentage_minor = round(max_value_minor,-1)
+
+print(percentage_minor)
+
+max_value_major = max(find_highest_percentage_major)
+
+percentage_major = round(max_value_major,-1)
+
+print(percentage_major)
+
+
+print("---")
+
+for note in {
+        "47":"B",
+        "46":"A#/Bb",
+        "45":"A",
+        "44":"G#/Ab",
+        "43":"G",
+        "42":"F#/Gb",
+        "41":"F",
+        "40":"E",
+        "39":"D#/Eb",
+        "38":"D",
+        "37":"C#/Db",
+        "36":"C"
+}:
+
+    test_note_name = midi_range[str(note)]
+
+    test_note = note
+
+    minor = scale_generator(int(test_note),[2,1,2,2,1,2,2])
+
+    major = scale_generator(int(test_note),[2,2,1,2,2,2,1])
+    
+    if scaleMatchPercentage(return_array_of_notes,minor) > percentage_minor:
+        print(test_note_name+" minor => "+str(scaleMatchPercentage(return_array_of_notes,minor)))
+
+    if scaleMatchPercentage(return_array_of_notes,major) > percentage_major:
+        print(test_note_name+" major => "+str(scaleMatchPercentage(return_array_of_notes,major)))
+ 
+   
+    pass
+
+
+
+# next stage can I teach the algorithm to work out the root note - the system has narrowed down the likelihood of the following being true
+
+# it is either B minor, G major, E minor , D major
+
+# How can a program detect "mood" in a song
