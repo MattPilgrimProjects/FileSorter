f523ac49ec2b06086060bcd35a7917c80c0cd459
MattPilgrimProjects <69475392+MattPilgrimProjects@users.noreply.github.com>
2020-10-23 23:32:10 +0100

    refactored key signature module

--- 3_3_return_key_signature.py
+++ 3_3_return_key_signature.py
@@ -1,125 +1,152 @@
 import app
 import library
 
-file_content = library.json.import_json("S:\\Midi-Library\\raw_midi_body_structure\\freemidi\\5698.json")
 
-midi_channels = library.json.import_json("midi.json")
+def return_key_signature(filename):
 
-diatonic_scale={
-    "C Major":["C","D","E","F","G","A","B"],
-    "D Major":["D", "E","F#/Gb", "G","A", "B", "C#/Db"],
-    "E Major":["E", "F#/Gb", "G#/Ab", "A", "B", "C#/Db", "D#/Eb"],
-    "F Major":["F","G","A","A#/Bb","C","D","E"],
-    "G Major":["G","A","B","C","D","E","F#/Gb"],
-    "A Major":["A","B","C#/Db","D","E","F#/Gb","G#/Ab"],
-    "B Major":["B", "C#/Db", "D#/Eb", "E", "F#/Gb", "G#/Ab", "A#/Bb"]
-}
+    library.comment.returnMessage("Processing:"+filename)
 
-minor_scales={
-    "B Minor":["B","C#/Db","D", "E","F#/Gb", "G","A"]
-}
+    file_content = library.json.import_json(filename)
 
-full_chord_array=[]
-full_notes_list=[]
-return_all_notes=[]
-full_list_of_notes_after_max_found=[]
+    midi_channels = library.json.import_json("midi.json")
 
-for channel,note_array in file_content.items():
+    diatonic_scale={
+        "C Major":["C","D","E","F","G","A","B"],
+        "D Major":["D", "E","F#/Gb", "G","A", "B", "C#/Db"],
+        "E Major":["E", "F#/Gb", "G#/Ab", "A", "B", "C#/Db", "D#/Eb"],
+        "F Major":["F","G","A","A#/Bb","C","D","E"],
+        "G Major":["G","A","B","C","D","E","F#/Gb"],
+        "A Major":["A","B","C#/Db","D","E","F#/Gb","G#/Ab"],
+        "B Major":["B", "C#/Db", "D#/Eb", "E", "F#/Gb", "G#/Ab", "A#/Bb"]
+    }
 
-    most_used_note_array=[]
+    minor_scales={
+        "B Minor":["B","C#/Db","D", "E","F#/Gb", "G","A"]
+    }
 
-    for note,count in note_array.items():
-        most_used_note_array.append(int(count))
-        pass
+    full_chord_array=[]
+    full_notes_list=[]
+    return_all_notes=[]
+    full_list_of_notes_after_max_found=[]
 
-    
+    for channel,note_array in file_content.items():
 
-    if most_used_note_array:
+        most_used_note_array=[]
 
         for note,count in note_array.items():
-            if int(count) == max(most_used_note_array):
-      
-                full_list_of_notes_after_max_found.append(midi_channels[note])
+            most_used_note_array.append(int(count))
+            pass
+
+        
+
+        if most_used_note_array:
+
+            for note,count in note_array.items():
+                if int(count) == max(most_used_note_array):
+        
+                    full_list_of_notes_after_max_found.append(midi_channels[note])
+
+    
+            pass
+
+
+        most_common_scale=[]
+
+        midi_list=[]
+
+        for note,count in note_array.items():  
+
+            full_notes_list.append(note)
+
+            midi_list.append(midi_channels[note])      
+        
+        
+
+
+        count_array=[]
+            
+        for title,scale in diatonic_scale.items():
+            count_array.append(library.parser.match_percentage(scale,midi_list))
+            pass
+
+
+        closest_match = max(count_array)
+
+
+        for title,scale in diatonic_scale.items():
+
+            if int(library.parser.match_percentage(scale,midi_list))==int(closest_match):
+                most_common_scale.append(title)
+            else:
+                pass
+            pass
+
+        full_chord_array.extend(most_common_scale)
+
+
+    pass  
+
+
+    list_max_array=[] 
+    for key,count in library.parser.distinct(full_chord_array).items():
+
+        list_max_array.append(int(count))
+
+        pass
+
+    for key,count in library.parser.distinct(full_chord_array).items():
+        if max(list_max_array) == int(count):
+            major = key
+
+
+
+    return_all_notes=[]
+
+    for midi_note,count in library.parser.distinct(full_list_of_notes_after_max_found).items():
+        return_all_notes.append(count)
+        pass
+
+
+    return_all_notes_result=[]
+
+    for midi_note,count in library.parser.distinct(full_list_of_notes_after_max_found).items():
+
+        if count == max(return_all_notes):
+
+    
+
+            if midi_note in major:
+                return major
+                # print(diatonic_scale[major])
+            else:
+                return midi_note+" Minor"
+                # print(minor_scales[midi_note+" Minor"])
+                
+                pass
+
+    
+
+        pass
+
+
+
+array=[]
+
+for filename in library.scan.scan_file_recursively("S:\\Midi-Library\\raw_midi_body_structure\\*\\*.json"):
+
+    remove_filepath = library.parser.find_and_replace_array(filename,{
+        "S:\\Midi-Library\\raw_midi_body_structure\\":"",
+        ".json":""
+    })
+
+    data = remove_filepath.split("\\")
+
+    array.append({
+        "source":data[0],
+        "id":data[1],
+        "result":return_key_signature(filename)
+    })
+
+library.json.export_json("S:\\Midi-Library\\sources\\key_signature_list.json",array)
 
  
-        pass
-
-
-    most_common_scale=[]
-
-    midi_list=[]
-
-    for note,count in note_array.items():  
-
-        full_notes_list.append(note)
-
-        midi_list.append(midi_channels[note])      
-    
-    
-
-
-    count_array=[]
-        
-    for title,scale in diatonic_scale.items():
-        count_array.append(library.parser.match_percentage(scale,midi_list))
-        pass
-
-
-    closest_match = max(count_array)
-
-
-    for title,scale in diatonic_scale.items():
-
-        if int(library.parser.match_percentage(scale,midi_list))==int(closest_match):
-            most_common_scale.append(title)
-        else:
-            pass
-        pass
-
-    full_chord_array.extend(most_common_scale)
-
-
-pass  
-
-
-list_max_array=[] 
-for key,count in library.parser.distinct(full_chord_array).items():
-
-    list_max_array.append(int(count))
-
-    pass
-
-for key,count in library.parser.distinct(full_chord_array).items():
-    if max(list_max_array) == int(count):
-        major = key
-
-
-
-return_all_notes=[]
-
-for midi_note,count in library.parser.distinct(full_list_of_notes_after_max_found).items():
-    return_all_notes.append(count)
-    pass
-
-
-return_all_notes_result=[]
-
-for midi_note,count in library.parser.distinct(full_list_of_notes_after_max_found).items():
-
-    if count == max(return_all_notes):
-
-  
-
-        if midi_note in major:
-            print(major)
-            print(diatonic_scale[major])
-        else:
-            print(midi_note+" Minor")
-            print(minor_scales[midi_note+" Minor"])
-            
-            pass
-
-   
-
-    pass
-
---
+++ scatter.py
@@ -0,0 +1,11 @@
+import app
+import library
+import matplotlib.pyplot as plt
+
+for schema in library.json.import_json("S:\\Midi-Library\\sources\\key_signature_list.json"):
+    pass
+# x = [5,7,8,7,2,17,2,9,4,11,12,9,6]
+# y = [99,86,87,88,111,86,103,87,94,78,77,85,86]
+
+# plt.scatter(x, y)
+# plt.show()
